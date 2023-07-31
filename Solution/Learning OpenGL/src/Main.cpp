9c0f39ca1ac4eb0ab87ee6caccc3301b763ee630
Tom Jonker <tjj.jonker@gmail.com>
2023-07-31 14:13:55 +0200

    Create simple shaders and a shader program

--- Solution/Learning OpenGL/src/Main.cpp
+++ Solution/Learning OpenGL/src/Main.cpp
@@ -5,6 +5,8 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+void CheckAndLogShaderErrors(unsigned int shader);
+void CheckAndLogProgramErrors(unsigned int program);
 
 int main() {
 	glfwInit(); // Initialize glfw
@@ -29,6 +31,53 @@ int main() {
 	GLCall(glViewport(0, 0, 1600, 900));
 	GLCall(glfwSetFramebufferSizeCallback(window, framebuffer_size_callback));
 
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f,
+	};
+
+	unsigned int VBO;
+	GLCall(glGenBuffers(1, &VBO)); // Vertex Buffer Object
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, VBO));
+	GLCall(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW));
+
+	const char* vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	GLCall(glShaderSource(vertexShader, 1, &vertexShaderSource, NULL));
+	GLCall(glCompileShader(vertexShader));
+	CheckAndLogShaderErrors(vertexShader);
+
+	const char* fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\0";
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	GLCall(glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL));
+	GLCall(glCompileShader(fragmentShader));
+	CheckAndLogShaderErrors(fragmentShader);
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	GLCall(glAttachShader(shaderProgram, vertexShader));
+	GLCall(glAttachShader(shaderProgram, fragmentShader));
+	GLCall(glLinkProgram(shaderProgram));
+	CheckAndLogProgramErrors(shaderProgram);
+	glUseProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	// Render loop
 	while (!glfwWindowShouldClose(window)) {
@@ -55,5 +104,26 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		GLCall(glfwSetWindowShouldClose(window, true));
+	}
+}
+
+void CheckAndLogShaderErrors(unsigned int shader) {
+	int success;
+	char infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "[SHADER COMPILE ERROR]: " << infoLog << std::endl;
+	}
+}
+
+void CheckAndLogProgramErrors(unsigned int program) {
+	int success;
+	char infoLog[512];
+
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(program, 512, NULL, infoLog);
 	}
 }
