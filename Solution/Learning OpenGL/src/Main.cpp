80de63d3acabbd1004965fe061f448addd1ddfa1
Tom Jonker <tjj.jonker@gmail.com>
2023-08-01 00:47:21 +0200

    Added texturing

--- Solution/Learning OpenGL/src/Main.cpp
+++ Solution/Learning OpenGL/src/Main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "Debugging/debugger.h"
 #include "Shader/Shader.h"
+#include "vendor/stb_lib/stb_lib.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -34,13 +35,16 @@ int main() {
 
 
 	float vertices[] = {
-		 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-		 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 	};
 
 	unsigned int indices[] = {
-		0, 1, 2
+		0, 1, 3,
+		1, 2, 3,
 	};
 
 	unsigned int vao;	// Vertex Array Objects -> Keeps track of all
@@ -53,19 +57,68 @@ int main() {
 	GLCall(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW));
 
 	unsigned int ebo;
-	GLCall(glGenBuffers(1, &ebo));	// Element Buffer Object -> Keeps track of indeces
+	GLCall(glGenBuffers(1, &ebo));	// Element Buffer Object -> Keeps track of indices
 	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo));
 	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW));
 
-	GLCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0));
+	GLCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0));
 	GLCall(glEnableVertexAttribArray(0));
 
-	GLCall(glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float))));
+	GLCall(glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float))));
 	GLCall(glEnableVertexAttribArray(1));
 
+	GLCall(glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float))));
+	GLCall(glEnableVertexAttribArray(2));
+
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	
+	unsigned int texture1;
+	GLCall(glGenTextures(1, &texture1));
+	GLCall(glBindTexture(GL_TEXTURE_2D, texture1));
+
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR));
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("src/textures/texture.png", &width, &height, &nrChannels, 0);
+	if (data) {
+		GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data));
+		GLCall(glGenerateMipmap(GL_TEXTURE_2D));
+	}
+	else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	unsigned int texture2;
+	GLCall(glGenTextures(1, &texture2));
+	GLCall(glBindTexture(GL_TEXTURE_2D, texture2));
+
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR));
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+
+	stbi_set_flip_vertically_on_load(true);
+	data = stbi_load("src/textures/container.jpg", &width, &height, &nrChannels, 0);
+	if (data) {
+		GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data));
+		GLCall(glGenerateMipmap(GL_TEXTURE_2D));
+	}
+	else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
 
 	Shader shader("src/shaders/vertexShader.glsl", "src/shaders/fragmentShader.glsl");
+
+	shader.Bind();
+	shader.SetInt("texture1", 0);
+	shader.SetInt("texture2", 1);
+
 
 	// Render loop
 	while (!glfwWindowShouldClose(window)) {
@@ -76,7 +129,11 @@ int main() {
 		GLCall(glClearColor(0.2f, 0.3f, 0.3f, 1.0f));
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
-		shader.Bind();
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
 
 		GLCall(glBindVertexArray(vao));
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0));
--- Solution/Learning OpenGL/src/Shader/Shader.h
+++ Solution/Learning OpenGL/src/Shader/Shader.h
@@ -22,10 +22,10 @@ public:
 
 	void Bind();
 
-	void SetBool(const std::string &name, bool v0);
-	void SetInt(const std::string &name, int v0);
-	void SetFloat(const std::string &name, float v0);
-	void Set4f(const std::string &name, float v0, float v1, float v2, float v3);
+	void SetBool(const std::string& name, bool v0);
+	void SetInt(const std::string& name, int v0);
+	void SetFloat(const std::string& name, float v0);
+	void Set4f(const std::string& name, float v0, float v1, float v2, float v3);
 
 private:
 	std::string ReadShader(const char* shaderPath);
